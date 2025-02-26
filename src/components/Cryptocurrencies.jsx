15b027c17e00d18b6d4e54a5a6607ceb461558a2
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-02-27 00:07:17 +0100

    change

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import millify from 'millify'
+import {card, row, col, input} from 'antd'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
   return (
