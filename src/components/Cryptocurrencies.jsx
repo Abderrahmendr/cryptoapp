4a90a02c9c6cb001fed8e017fdd4535afa835787
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-03-04 00:31:11 +0100

    change

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,19 +1,38 @@
 import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import millify from 'millify'
-import {card, Row, col, input} from 'antd'
+import {Card, Row, Col, input} from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
    
 
 
 const Cryptocurrencies = () => {
   const {data:cryptosList, isFetching} = useGetCryptosQuery();
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coin)
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
 
   return (
     <div>
       <>
         <Row gutters={[32, 32]} className="crypto-card-container">
+          {cryptos.map((currency)=>{
+            <Col xs={24} lg={6} className="crypto-card" key={currency.id}>
+              <Link to={`/crypto/${currency.id}` }>
+                 <Card
+                 title={`${currency.rank}. ${currency.name}`}
+                 extra={<img className="crypto-image"src={currency.iconUrl}/>}
+                 hoverable
+                 >
+                  <p>Price:{millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.MarketCap)}</p>
+                  <p>Daily Change: {millify(currency.DailyChange)}</p>
+
+                 </Card>
+
+              
+              </Link>
+            
+            </Col>
+          })}
 
         </Row>
       </>
