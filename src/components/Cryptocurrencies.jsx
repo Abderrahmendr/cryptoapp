fc106e3de8362bad17f09137641f1bbe1cffc1cb
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-03-03 07:51:29 +0100

    change

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import millify from 'millify'
 import {card, row, col, input} from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+   
+
 
 const Cryptocurrencies = () => {
+  const {data:cryptosList, isFetching} = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coin)
+
   return (
     <div>
       <p>cr</p>
