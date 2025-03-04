45586b7cbbd575a957de2fed611de715f18deb30
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-03-05 00:14:02 +0100

    change

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,35 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import millify from 'millify';
-import { Card, Row, Col } from 'antd';
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import React ,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import millify from 'millify'
+import {Card, Row, Col, Input} from 'antd'
+import { useGetCryptosQuery } from '../services/cryptoApi'
+   
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+
+const Cryptocurrencies = (simplified) => {
+  const count = simplified ? 10 : 100;
+  const {data: cryptosList, isFetching} = useGetCryptosQuery();
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
-
- 
-  return (
+   return (
     <div>
-      <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
-          <Col xs={24} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
-              <Card
-                title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name} />}
-                hoverable
-              >
-                <p>Price: {millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <p>Daily Change: {millify(currency.change)}</p>
-              </Card>
-            </Link>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
-};
+    <Row gutter={[32, 32]} className="crypto-card-container">
+      {cryptos?.map((currency) => (
+        <Col xs={24} lg={6} className="crypto-card" key={currency.id}>
+          <Link to={`/crypto/${currency.id}`}>
+            <Card
+              title={`${currency.rank}. ${currency.name}`}
+              extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name} />}
+              hoverable
+            >
+              <p>Price: {millify(currency.price)}</p>
+              <p>Market Cap: {millify(currency.marketCap)}</p>
+              <p>Daily Change: {millify(currency.change)}%</p>
+            </Card>
+          </Link>
+        </Col>
+      ))}
+    </Row>
+  </div>
+  )
+}
 
-export default Cryptocurrencies;
+export default Cryptocurrencies
