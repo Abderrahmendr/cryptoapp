72801346502f5c6882c80a3e47c988400e86c1f7
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-03-09 23:57:01 +0100

    change

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,11 +6,25 @@ const {Text, Title} = Typography;
 const {option} = Select;
 
  const News = ({simplified}) => {
-  const {data : cryptoNews} = useGetCryptoNewsQuery({newsCategory:'cryptocurrency', count})
+  const {data : cryptoNews} = useGetCryptoNewsQuery({newsCategory:'cryptocurrency', count:simplified ? 6 :12 })
+  if(cryptoNews?.value) return 'Loading...'
   return (
-    <div>
-      <p>vh</p>
-    </div>
+     <Row gutter={[24,24]}>
+      {cryptoNews.value.map((news, i)=>
+      <Col xs={24} sm={12} ls={8} key={i} >
+        <Card hoverable className='news-card'>
+          <a href={news.url} target='_blank' rel='noreferrer'>
+                 <div className='news-image-container'>
+
+                         <Title className='news-title' level={4}>{news.name}</Title>
+                 </div>
+          </a>
+
+        </Card>
+      
+      </Col>
+      )}
+     </Row>
   )
 }
 
