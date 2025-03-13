80e94193a1b6756e7e98da2189d48be26cac5b73
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-03-13 02:30:31 +0100

    change

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+{/*import React, { useState } from 'react';
 import HTMLReactParser from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
 
 const {Title, Text} = Typography;
 const {Option} = Select;
 
 const CryptoDetails = () => {
-  const {coindId} = useParams();
-  return (
+  const {coinId} = useParams();
+  const [timePeriod, setTimePeriod] = useState['7d']
+  const [data, isFetching] = useGetCryptoDetailsQuery(coinId)
+ const cryptoDetails = data?.data?.coin;
+ 
+ 
+ const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+ const stats = [
+   { title: 'Price to USD', value: `$ ${CryptoDetails?.price && millify(CryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
+   { title: 'Rank', value: CryptoDetails?.rank, icon: <NumberOutlined /> },
+   { title: '24h Volume', value: `$ ${CryptoDetails?.volume && millify(CryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+   { title: 'Market Cap', value: `$ ${CryptoDetails?.marketCap && millify(CryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
+   { title: 'All-time-high(daily avg.)', value: `$ ${CryptoDetails?.allTimeHigh?.price && millify(CryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+ ];
+
+ const genericStats = [
+   { title: 'Number Of Markets', value: CryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
+   { title: 'Number Of Exchanges', value: CryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+   { title: 'Aprroved Supply', value: CryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+   { title: 'Total Supply', value: `$ ${CryptoDetails?.supply?.total && millify(CryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
+   { title: 'Circulating Supply', value: `$ ${CryptoDetails?.supply?.circulating && millify(CryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
+ ];
+ return (
     <div>
-      
+            
     </div>
   )
 }
 
 export default CryptoDetails
+*/}
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,22 +1,21 @@
-import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
-    'x-rapidapi-key': '71300e2f51msh493a2407e7ff7b6p16992ajsn965c2ef0f14b',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'X-BingApis-SDK': 'true'
-}
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': '71300e2f51msh493a2407e7ff7b6p16992ajsn965c2ef0f14b',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+};
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
-const createRequest = (url) => ({ url, headers : cryptoNewsHeaders})
-
-export const cryptoNewsApi = createApi ({
-    reducerPath:'cryptoNewsApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-   endpoints: (builder) => ({
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://bing-news-search1.p.rapidapi.com' }),
+  endpoints: (builder) => ({
     getCryptoNews: builder.query({
-        query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),    })
- })
-})
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
 export const { useGetCryptoNewsQuery } = cryptoNewsApi;
