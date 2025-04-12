9874906fac95dc5b132fafa0e77e4249dc3f3811
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2025-04-12 16:39:11 +0100

    change

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -12,7 +12,7 @@ const Exchanges = () => {
   const { data, isFetching } = useGetExchangesQuery();
  // const exchangesList = data?.data?.exchanges;
 
-   if (isFetching) return <Loader />;
+    
      if (!Exchanges?.value) return <Loader />;
    
 
